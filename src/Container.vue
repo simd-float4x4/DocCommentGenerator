891f757dcc874601af93a23c09814dc15e1173a1
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 13:34:50 +0900

    fix: src/components/Container.vueを1つ上のディレクトリであるsrc/Container.vueに移動させる

---
+++ src/Container.vue
@@ -0,0 +1,7 @@
+<container>
+    <div class="container">
+        <h1 class="mt-5">Title</h1>
+        <p class="lead">Description</p>
+        <p>sub-description <a href="">URL</a></p>
+    </div>
+</container>
