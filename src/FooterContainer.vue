575b6a0cebb93f2acaf52ba7e23b577aa2a1427c
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 15:05:41 +0900

    fix: FooterContainerのvueファイルを記述

---
+++ src/FooterContainer.vue
@@ -0,0 +1,12 @@
+<template>
+    <FooterContainer></FooterContainer>
+</template>
+
+<script>
+import FooterContainer from './components/FooterContainer.vue'
+export default {
+  components: {
+    FooterContainer
+  }
+}
+</script>
