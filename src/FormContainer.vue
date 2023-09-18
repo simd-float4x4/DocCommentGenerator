60bbb374ec7f1f12cee21047e1734a227e16c445
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 15:56:52 +0900

    create: Title.vueの新規追加／fix: ページにタイトルが表示されるよう修正

--- src/FormContainer.vue
+++ src/FormContainer.vue
@@ -1,13 +1,19 @@
 <template>
+  <Title></Title>
   <div class="container">
+    <Form></Form>
+    <Form></Form>
     <Form></Form>
   </div>
 </template>
 
 <script>
+import Title from './components/Title.vue'
 import Form from './components/Form.vue'
+
 export default {
   components: {
+    Title,
     Form
   }
 }
---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mt-5">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "DocDommentGeneratorForSwift"
+        }
+    }
+}
+</script>
