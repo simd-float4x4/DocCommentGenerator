15e836951ed87e1cf5c7fc4eefeefd12bb3bcef4
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 14:52:59 +0900

    fix: main.jsにおいて、index.htmlからFooter.vueが呼ばれるように修正

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import Container from './Container.vue'
+import Footer from './Footer.vue'
 
 createApp(Container).mount('#container')
+createApp(Footer).mount('#footer')
