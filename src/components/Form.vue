22617a7be586f8e2d4613894e94a6dc4318462e1
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 13:36:56 +0900

    fix: Container.vueの実装内容を全てForm.Vueに移動

---
+++ src/components/Form.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h1 class="mt-5">Title</h1>
+        <p class="lead">Description</p>
+        <p>sub-description <a href="">URL</a></p>
+    </div>
+</template>
