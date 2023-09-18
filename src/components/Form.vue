ac33116089bcea04d6b4e39d5f9a302a06fa87ef
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 14:42:10 +0900

    fix: Form.vueについてパラメータを<script>から取得できるように修正

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="container">
-        <h1 class="mt-5">Title</h1>
-        <p class="lead">Description</p>
-        <p>sub-description <a href="">URL</a></p>
+        <h1 class="mt-5">{{ Title }}</h1>
+        <p class="lead">{{ Description }}</p>
+        <p>{{ subText }}<a href="">{{ url }}</a></p>
     </div>
 </template>
+
+<script>
+export default {
+    data(){ 
+        return {
+            title: "Title",
+            description: "Description",
+            subText: "SubText",
+            url: "URL"
+        }
+    }
+}
+</script>
