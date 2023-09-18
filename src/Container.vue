0fc5d3111ff3562cc3a2d8caeb86ac5325ad9ac4
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 15:00:54 +0900

    delete: Container.vueから<footer>に関する記述を削除

--- src/Container.vue
+++ src/Container.vue
@@ -2,17 +2,13 @@
   <div class="container">
     <Form></Form>
   </div>
-  <Footer></Footer>
 </template>
 
 <script>
 import Form from './components/Form.vue'
-import Footer from './components/Footer.vue'
-
 export default {
   components: {
-    Form,
-    Footer
+    Form
   }
 }
 </script>
