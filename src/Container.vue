2cef7109dbe0949703e2d5caa754f170c2e64483
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 14:59:01 +0900

    fix: Container.vueからFooter.vueが呼ばれるように修正

--- src/Container.vue
+++ src/Container.vue
@@ -2,14 +2,17 @@
   <div class="container">
     <Form></Form>
   </div>
+  <Footer></Footer>
 </template>
 
 <script>
 import Form from './components/Form.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   components: {
-    Form
+    Form,
+    Footer
   }
 }
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,17 @@
 <template>
     <footer class="footer mt-auto py-3">
         <div class="container text-center">
-            <span class="text-muted">©︎ Created By Shumpei Horiuchi, 2023</span>
+            <span class="text-muted">{{ copyRights }}</span>
         </div>
     </footer>
 </template>
+
+<script>
+export default {
+    data(){ 
+        return {
+            copyRights: "©︎ Created By Shumpei Horiuchi, 2023"
+        }
+    }
+}
+</script>
