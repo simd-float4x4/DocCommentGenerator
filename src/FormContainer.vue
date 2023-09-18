aa446aee56ae4affade437d336aa0f7a55e48f21
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 20:15:07 +0900

    fix: 説明文の配列データをfill

--- src/FormContainer.vue
+++ src/FormContainer.vue
@@ -24,7 +24,9 @@ export default {
     return {
       items: [
         {id: 1, title: "1. INPUT YOUR CODE HERE", description: "メソッドの1行目を入力してください。", subText: "例) func fetchUserData(id: Int, name: String) -> UserModel { ", url: null },
-        {id: 2, title: "2. INPUT YOUR CODE HERE", description: "メソッドの1行目を入力してください。", subText: "例) func fetchUserData(id: Int, name: String) -> UserModel { ", url: null },
+        {id: 2, title: "2. INPUT YOUR DESCRIPTION HERE", description: "このメソッドはどんなメソッドですか？", subText: "例) 引数の値でResponse構造体を作成する。引数がnilの場合はランダムに値を作成する。", url: null },
+        {id: 3, title: "3. CHECK YOUR VARIABLE HERE", description: "生成された引数の一覧が正しいか確認してください。", subText: "なお、+ボタン -ボタンで要素の数を調整できます。", url: null },
+        {id: 4, title: "4. CHECK YOUR RETURN VALUE", description: "帰り値/戻り値/リターン値が存在していることを確認してください。", subText: "例）　return WeatherModel 　// 帰り値が必要", url: null },
       ]
     }
   }
