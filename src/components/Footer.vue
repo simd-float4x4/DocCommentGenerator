91e2bce4d65aa46380c4aa40d54bb3c05eb89547
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 14:55:00 +0900

    fix: index.htmlから<footer>要素をFooter.vueに移植

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="footer mt-auto py-3">
+        <div class="container text-center">
+            <span class="text-muted">©︎ Created By Shumpei Horiuchi, 2023</span>
+        </div>
+    </footer>
+</template>
