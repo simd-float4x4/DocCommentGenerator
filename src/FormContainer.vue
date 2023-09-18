99aee73d5f37c29c27b7bfb4d0f29bc351770399
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2023-09-18 20:27:14 +0900

    fix: ステップ1の説明文を改行

--- src/FormContainer.vue
+++ src/FormContainer.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <ul class="list-group">
       <li class="list-group-item" v-for="item in items" :key="item.id">
-        <div class="container">
+        <div class="container" style="white-space: pre-wrap;">
           <FormView :title="item.title" :description="item.description" :subText="item.subText" :url="item.url"></FormView>
         </div>
       </li>
@@ -23,7 +23,7 @@ export default {
   data() {
     return {
       items: [
-        {id: 1, title: "1. INPUT YOUR CODE HERE", description: "メソッドの1行目を入力してください。", subText: "例) func fetchUserData(id: Int, name: String) -> UserModel { ", url: null },
+        {id: 1, title: "1. INPUT YOUR CODE HERE", description: "メソッドの1行目を入力してください。\n※ なお、Opening Brackets( { }まで必ず含めてください。)", subText: "例) func fetchUserData(id: Int, name: String) -> UserModel { ", url: null },
         {id: 2, title: "2. INPUT YOUR DESCRIPTION HERE", description: "このメソッドはどんなメソッドですか？", subText: "例) 引数の値でResponse構造体を作成する。引数がnilの場合はランダムに値を作成する。", url: null },
         {id: 3, title: "3. CHECK YOUR VARIABLE HERE", description: "生成された引数の一覧が正しいか確認してください。", subText: "なお、+ボタン -ボタンで要素の数を調整できます。", url: null },
         {id: 4, title: "4. CHECK YOUR RETURN VALUE", description: "帰り値/戻り値/リターン値が存在していることを確認してください。", subText: "例）　return WeatherModel 　// 帰り値が必要", url: null },
